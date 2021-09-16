c0c84bea28ab721dcd09e8bbd69904143c53d695
LabDiMartino <labdimartino@gmail.com>
2021-09-16 10:48:33 +0100

    Keysight updates from 14/9

--- nplab/instrument/electronics/Keysight_DSOX1204A.py
+++ nplab/instrument/electronics/Keysight_DSOX1204A.py
@@ -10,10 +10,15 @@ Class for Keysight DSOX1204A digital oscilloscope.
 """
 
 """import modules"""
+
+
 from nplab.instrument.visa_instrument import VisaInstrument
 import pyvisa
-import numpy as np
 import struct
+import matplotlib.pyplot as plt
+import numpy as np
+  
+
 
 
 class Keysight_DSOX1204A(VisaInstrument):
@@ -48,6 +53,9 @@ class Keysight_DSOX1204A(VisaInstrument):
         
     def trigger_edge(self):
         self.write(':TRIGger:MODE EDGE')
+        
+    def trigger_edge_polarity(self):    
+        self.write(":TRIGger:EDGE:SLOPe POSitive")
             
     def autoscale(self):
         self.write(':AUToscale') #The :AUToscale command evaluates all input signals and sets the correct
@@ -83,63 +91,62 @@ class Keysight_DSOX1204A(VisaInstrument):
         self.write(':WAVeform:POINts:MODE RAW')
     
     def set_waveform_points(self): # sets number of points
-        self.write(':WAVeform:POINts 18000')
+        self.write(':WAVeform:POINts 1800')
         
     def waveform_source(self,waveform_channel_source):
         self.write(':WAVeform:SOURce CHANnel{}'.format(waveform_channel_source))
         
         
-    def waveform_data_format(self,data_format):
-        self.write(':WAVeform:FORMat {}'.format(data_format))
-        
-    def read_waveform_data(self):
-        """stuff"""
-        #self.query_binary_values('WAV:DATA?', datatype='s')                  
-        self.parsed_query('WAV:DATA?', datatype='s')                  
-        
+    def waveform_data_format(self):
+        self.write(':WAVEFORM:FORMAT BYTE')
+
+    
 if __name__ == '__main__':
     
     rm = pyvisa.ResourceManager() # call resource manager from module pyvisa
     rm.list_resources() #lists all resources available in case one forgets the resoucrce address
     myScope = rm.open_resource('USB0::0x2A8D::0x0386::CN60476268::0::INSTR') #opens oscilloscope
-    #myScope.timeout = 25000 #to set a timeout for the device operation
+    myScope.timeout = 25000 #to set a timeout for the device operation
 
      
     myDSOX1204A = Keysight_DSOX1204A()
   # myDSOX1204A.autoscale() # optional, creates a time lag for other instructions to execute and throws error
-  # myDSOX1204A.channel_display_on(['1']) # channel 1 display alone is on
+    myDSOX1204A.channel_display_on(['1']) # channel 1 display alone is on
     myDSOX1204A.trigger_source()
     myDSOX1204A.trigger_edge()
-    myDSOX1204A.set_vertical_scale('20')
+    myDSOX1204A.trigger_edge_polarity()
+  #  myDSOX1204A.set_vertical_scale('20')
     myDSOX1204A.acquire_type('NORMal') # other options are 'AVERage','HRESolution','PEAK'
     #myDSOX1204A.run()
-    hi = myDSOX1204A.single_run()
+    #myDSOX1204A.single_run()
     #myDSOX1204A.digitize(['1']) # digitized only channel 1.
     myDSOX1204A.waveform_mode() # set waveform mode
     myDSOX1204A.set_waveform_points()
     myDSOX1204A.waveform_source(['1'])
-    myDSOX1204A.waveform_data_format('BYTE') # can be set to BYTE, WORD, ASCii
-   # myDSOX1204A.acquire_mode('RTIMe') # other options are segmented. Option selected is RealTime
+    myDSOX1204A.waveform_data_format() # can be set to BYTE, WORD, ASCii
+    myDSOX1204A.acquire_mode('RTIMe') # other options are segmented. Option selected is RealTime
 
-    values = myScope.query_binary_values('WAV:DATA?', datatype='s')
-    print(values)
-
+    num_cycles = 2
+    values_all_cycles = np.array([])
+    for cycles in range(num_cycles):    
+        values_raw_binary = myScope.query_binary_values('WAV:DATA?', datatype='s')
+        values_unpacked = struct.unpack("%dB" % len(values_raw_binary[0]), values_raw_binary[0])
+        values_all_cycles = np.append(values_all_cycles,values_unpacked)
+        
     x_increment = myDSOX1204A.write(":WAVeform:XINCrement?")[0]
     x_origin = myDSOX1204A.write(":WAVeform:XORigin?")[0]
     y_increment = myDSOX1204A.write(":WAVeform:YINCrement?")[0]
     y_origin = myDSOX1204A.write(":WAVeform:YORigin?")[0]
     y_reference = myDSOX1204A.write(":WAVeform:YREFerence?")[0]
    
-    values2 = struct.unpack("%dB" % len(values[0]), values[0])
-    
     f = open("waveform_data.csv", "w")
-    for i in range(0, len(values2) - 1):
+    for i in range(0, len(values_all_cycles) - 1):
         time_val = x_origin + (i * x_increment)
-        voltage = ((values2[i] - y_reference) * y_increment) + y_origin
+        voltage = (((int(values_all_cycles[i]) - y_reference) * y_increment) + y_origin)
         f.write("%E, %f\n" % (time_val, voltage))
     f.close()
-
     
+    plt.plot(values_all_cycles)
 
 
 
